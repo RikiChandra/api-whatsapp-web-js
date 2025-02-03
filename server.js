8c1bbdc13d1f7e0970a222d8a44b421598e3fc99
RikiChandra <49185211+RikiChandra@users.noreply.github.com>
2025-02-03 08:57:16 +0700

    update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const socketIO = require('socket.io');
 const http = require('http');
 
 // initial instance
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
@@ -21,7 +21,7 @@ const client = new Client({
             '--disable-accelerated-2d-canvas',
             '--no-first-run',
             '--no-zygote',
-            '--single-process', // <- this one doesn't work in Windows
+            '--single-process',
             '--disable-gpu'
         ],
     }
